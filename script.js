82026ee3c38f96079e0c480db61a6140c38a49f2
geekbhanu <bhanu.sriva25@gmail.com>
2025-06-29 18:26:11 +0530

    local storage handle

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const todoInput = document.getElementById('todo-input');
+document.addEventListener('DOMContentLoaded', () => {
+  const todoInput = document.getElementById('todo-input');
 const addTaskButton = document.getElementById('todo-input');
 const todoList = document.getElementById('todo-list');
 
-let tasks = [];
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
+tasks.forEach(tasks => renderTask(tasks));
 addTaskButton.addEventListener('click', ()=>{
   const taskText = todoInput.value.trim()
   if (taskText === "") return;
@@ -13,6 +15,14 @@ addTaskButton.addEventListener('click', ()=>{
     completed: false    
   }
   tasks.push(newTask);
+  saveTasks();
   todoInput.value = ""; //clear input field
   console.log(tasks);
 })
+function renderTask(task){
+console.log(task);
+}
+function saveTasks() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+});
